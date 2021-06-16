77067d3ac1e506404de43b4c578a2aefeb37b608
Georgi D. Sotirov <gdsotirov@gmail.com>
2021-06-16 23:38:46 +0300

    Remove inline <script> elements
    Remove inline <script> elements to avoid violation of script-src CSP
    (Content Security Policy) directive.
    
    See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/script-src

---
+++ js/map.js
@@ -0,0 +1,30 @@
+/**
+ * @file Generate visited countries map
+ * @author Georgi D. Sotirov <gdsotirov@gmail.com>
+ */
+
+var map = AmCharts.makeChart("mapdiv",{type: "map",theme: "dark",projection: "miller",
+panEventsEnabled : true,backgroundColor : "#000066",backgroundAlpha : 1,
+zoomControl: {zoomControlEnabled : true},
+dataProvider : {map : "worldHigh",getAreasFromMap : true,
+areas :[{"id": "AT","showAsSelected": true},
+        {"id": "BE","showAsSelected": true},
+        {"id": "BG","showAsSelected": true},
+        {"id": "DE","showAsSelected": true},
+        {"id": "FR","showAsSelected": true},
+        {"id": "GR","showAsSelected": true},
+        {"id": "HU","showAsSelected": true},
+        {"id": "IT","showAsSelected": true},
+        {"id": "MK","showAsSelected": true},
+        {"id": "NL","showAsSelected": true},
+        {"id": "PT","showAsSelected": true},
+        {"id": "RO","showAsSelected": true},
+        {"id": "RS","showAsSelected": true},
+        {"id": "ES","showAsSelected": true},
+        {"id": "TR","showAsSelected": true}]
+},
+areasSettings : {autoZoom : true,color : "#B4B4B7",colorSolid : "#cc3300",
+selectedColor : "#cc3300",outlineColor : "#666666",rollOverColor : "#ff9999",
+rollOverOutlineColor : "#000000"}
+});
+
